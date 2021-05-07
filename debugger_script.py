b80753b56e289501a8af53734aaa95722c7a1fa8
Zachary DeVito <zdevito@fb.com>
2021-05-07 09:52:58 -0700

    remove dead code

--- debugger_script.py
+++ debugger_script.py
@@ -1,15 +1,7 @@
 import lldb
 # load into lldb instance with:
 #   command script import debugger_script.py
-def on_special_break():
-    name_ptr, file_addr, file_size, load_bias = read_global_variable("__deploy_module_info")
-    file_name = read_string(name_ptr)
-    file_contents = read_mem(file_addr, file_size)
-    temp_file = TempFile(with_hint=file_name)
-    temp_file.write(file_contents)
-    lldb.debugger.HandleCommand(f"target module add {temp_file.name}")
-    lldb.debugger.HandleCommand(f"target module load -f {temp_file.name} {load_bias}")
-    tell_debugger_not_to_stop()
+
 target = lldb.debugger.GetSelectedTarget()
 print(target)
 bp = target.BreakpointCreateByRegex("__deploy_register_code")
